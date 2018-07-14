805f597aec41229822e9279451a0747003297002
Ryan Burdett <rburdett@asu.edu>
2018-07-14 14:57:42 -0700

    Elements organized, implementing algebraic logic.

--- JScalc/JScalc/js/main.js
+++ JScalc/JScalc/js/main.js
@@ -14,5 +14,24 @@ function Add(x, y)
     return (x+y);
 }
 
+function Subtract(x,y)
+{
+    return (x-y);
+}
+
+function Multiply(x,y)
+{
+    return (x*y);
+}
+
+function Divide(x,y)
+{
+    return (x/y);
+}
+
+function Equals(x,y)
+{
+    //display result here
+}
 
 
